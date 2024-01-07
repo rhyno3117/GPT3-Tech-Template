0446df4c104d3166fe7d2310a7b08483d6d4c80e
ryan <rhynogolfer42@gmail.com>
2024-01-07 15:42:51 -0800

    Adds myfile.md

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
+
 
 //BEM -> Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div className="gpt__navbar">
+    <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+
       </div>
     </div>
   )
